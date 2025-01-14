74eede371b7cc9343f994cb70b3574e0caf9a0ee
Mega-Gorilla <hahahadesu@gmail.com>
2025-01-14 21:43:44 +0900

    init定義

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from .timecard_processor import TimecardProcessor
+from .gui import LogRedirector,ProcessThread,TimecardGUI
+
+__all__ = [
+    'TimecardProcessor',
+    'LogRedirector',
+    'ProcessThread',
+    'TimecardGUI'
+]
